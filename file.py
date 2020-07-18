ddee7354d4b3e4b5570a00a5d59c2e8d2c20a93a
Arex360 <49303647+Arex360@users.noreply.github.com>
2020-07-18 08:51:52 +0500

    Update file.py

--- file.py
+++ file.py
@@ -3,7 +3,7 @@ from detection import detect
 faceCas = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(1) # try -1, 0 , 1 in the case if you can't see video
 while True:
-    _,img = cap.read()
+    ret,img = cap.read()
     img = detect(img,faceCas)
     cv2.imshow('img',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
